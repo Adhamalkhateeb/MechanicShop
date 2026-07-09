8d7dcaf517141e57cc32a9debd6b7525493f71e9
Adhamalkhateeb <adhamalkhateeb2006@gmail.com>
2026-07-09 18:45:13 +0300

    feat(api): add general problems handler for controller and endpoint

---
+++ src/MechanicShop.Api/Extensions/ProblemExtensions.cs
@@ -0,0 +1,48 @@
+using MechanicShop.Domain.Common.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MechanicShop.Api.Extensions;
+
+public static class ProblemExtensions
+{
+    public static IResult ToProblem(List<Error> errors)
+    {
+        if (!errors.Any())
+        {
+            return Results.Problem();
+        }
+
+        if (errors.All(e => e.Type == ErrorKind.Validation))
+        {
+            return ValidationProblem(errors);
+        }
+
+        return Results.Problem();
+    }
+
+    private static IResult ToProblem(Error error)
+    {
+        var statusCode = error.Type switch
+        {
+            ErrorKind.Conflict => StatusCodes.Status409Conflict,
+            ErrorKind.Validation => StatusCodes.Status400BadRequest,
+            ErrorKind.NotFound => StatusCodes.Status404NotFound,
+            ErrorKind.Unauthorized => StatusCodes.Status401Unauthorized,
+            ErrorKind.Forbidden => StatusCodes.Status403Forbidden,
+            _ => StatusCodes.Status500InternalServerError,
+        };
+
+        return Results.Problem(statusCode: statusCode, title: error.Description);
+    }
+
+    private static IResult ValidationProblem(List<Error> errors)
+    {
+        var validationErrors = errors.ToDictionary(e => e.Code, e => new[] { e.Description });
+
+        var problemDetails = new ValidationProblemDetails(validationErrors)
+        {
+            Status = StatusCodes.Status400BadRequest,
+        };
+        return Results.Json(problemDetails, statusCode: StatusCodes.Status400BadRequest);
+    }
+}
